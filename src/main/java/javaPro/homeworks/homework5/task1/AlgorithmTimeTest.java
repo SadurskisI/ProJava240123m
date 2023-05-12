51c074901cbce527ceede3edb3a2441fb857b8c4
SadurskisI <igors.sadurskis@gmail.com>
2023-05-12 13:56:18 +0300

    Java pro lessons

--- src/main/java/javaPro/homeworks/homework5/task1/AlgorithmTimeTest.java
+++ src/main/java/javaPro/homeworks/homework5/task1/AlgorithmTimeTest.java
@@ -16,55 +16,55 @@ public class AlgorithmTimeTest {
         int listSize = list.size();
 
 //Перебрать лист с помощью for-each, в теле цикла каждое значение листа записать в переменную temp.
-        long time11 = System.currentTimeMillis();
+        long time1 = System.currentTimeMillis();
         for (Integer number : list) {
             int tmp = number;
         }
-        long time12 = System.currentTimeMillis();
-        System.out.println((time12 - time11));
+        long time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1));
 
 //        Сделать то же самое с помощью классического for.
-        time11 = System.currentTimeMillis();
+        time1 = System.currentTimeMillis();
         for (int i = 0; i < list.size(); i++) {
             int tmp = list.indexOf(i);
         }
-        time12 = System.currentTimeMillis();
-        System.out.println((time12 - time11));
+        time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1));
 
 //Сделать то же самое с помощью классического for, но сначала вынести размер листа в отдельную переменную,
 // а потом эту переменную использовать внутри условия цикла.
-        time11 = System.currentTimeMillis();
+        time1 = System.currentTimeMillis();
         for (int i = 0; i < listSize; i++) {
             int tmp = list.indexOf(i);
         }
-        time12 = System.currentTimeMillis();
-        System.out.println((time12 - time11));
+        time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1));
 
 //Сделать то же самое, что и в пункте 5, но перебрать лист с конца до начала.
-        time11 = System.currentTimeMillis();
+        time1 = System.currentTimeMillis();
         for (int i = listSize - 1; i >= 0; i--) {
             int tmp = list.indexOf(i);
         }
-        time12 = System.currentTimeMillis();
-        System.out.println((time12 - time11));
+        time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1));
 
 //Сделать то же самое, но используя Iterator.
-        time11 = System.currentTimeMillis();
+        time1 = System.currentTimeMillis();
         Iterator<Integer> iterator = list.iterator();
         while (iterator.hasNext()) {
             iterator.next();
         }
-        time12 = System.currentTimeMillis();
-        System.out.println((time12 - time11));
+        time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1));
 
 //Сделать то же самое, но используя ListIterator.
 //Для всех случаев 3-8 замерить время, в течение которого отрабатывает цикл
-        time11 = System.currentTimeMillis();
+        time1 = System.currentTimeMillis();
         ListIterator<Integer> listIterator = list.listIterator();
         while (listIterator.hasNext()) {
             listIterator.next();
         }
-        time12 = System.currentTimeMillis();
-        System.out.println((time12 - time11));
+        time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1));
     }
 }
