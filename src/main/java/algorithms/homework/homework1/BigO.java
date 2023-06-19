fc9ef6338d0ee9297eba2afef0e286300ac0c785
SadurskisI <igors.sadurskis@gmail.com>
2023-06-19 11:13:52 +0300

    Algorithms

---
+++ src/main/java/algorithms/homework/homework1/BigO.java
@@ -0,0 +1,44 @@
+package algorithms.homework.homework1;
+
+public class BigO {
+
+//    task#1
+//            if n == 1 THEN return
+//    FOR(i = 1; i <= n; i++) {
+//        FOR(j = 1; j <= n; j++) {
+//            //
+//        }
+//    }
+//
+//     ----------O(n^2)
+//
+//
+//    task#2
+//    FOR(i = n/2; i<=n;i++){
+//        FOR (j = 2; j <= n; j*2) {
+//            //
+//        }
+//    }
+
+//    ----------O(nlog(n))
+//
+//
+//    task#3
+//    FOR(i = 0; i<n;i++){
+//        FOR (j = n; j > i; j--) {
+//            //
+//        }
+//    }
+//
+//    -------------O(n^2)
+//
+//
+//    task#4
+//    numbers a=0, i=n;
+//    WHILE i>0 {
+//        a=a+i
+//        i=i/2
+//    }
+//}
+//    ----------log2(n)
+}
